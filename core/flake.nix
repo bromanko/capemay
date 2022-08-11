f3d28ad2b9268ebaef5fed5fe83e5176ee7daafe
Brian Romanko <hello@bromanko.com>
2022-08-10 21:14:04 -0700

    Add initial flake file

---
+++ core/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Cape May";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      packages = forAllSystems
+        (system: let pkgs = nixpkgsFor.${system}; in { default = pkgs.hello; });
+    };
+}
