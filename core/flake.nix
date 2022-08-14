10e59503f3b562b6372518560bbe8a994e13cac9
Brian Romanko <hello@bromanko.com>
2022-08-13 21:01:14 -0700

    Add sqitch dependency

--- core/flake.nix
+++ core/flake.nix
@@ -3,23 +3,28 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs = inputs@{ self, nixpkgs }:
     let
       supportedSystems =
         [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ (import ../nix/overlays/sqitch.nix) ];
+        });
     in {
       packages = forAllSystems
-        (system: let pkgs = nixpkgsFor.${system}; in { default = pkgs.hello; });
+        (system: let pkgs = nixpkgsFor.${system}; in { default = { }; });
 
       devShells = forAllSystems (system:
         let pkgs = nixpkgsFor.${system};
         in {
-          default =
-            pkgs.mkShell { buildInputs = with pkgs; [ dotnet-sdk mono ]; };
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ dotnet-sdk mono sqlite sqitch ];
+          };
         });
     };
 }
---
+++ nix/overlays/sqitch.nix
@@ -0,0 +1,49 @@
+# {
+# config,
+# lib,
+# pkgs,
+# shortenPerlShebang,
+# ...
+# }:
+
+self: super:
+
+let
+  pkgs = self.pkgs;
+  perlSqitch = pkgs.perlPackages.AppSqitch;
+  modules = with pkgs.perlPackages; [ DBDSQLite DBDPg ];
+in rec {
+  sqitch = pkgs.stdenv.mkDerivation {
+    pname = "sqitch";
+    version = perlSqitch.version;
+
+    nativeBuildInputs = [ pkgs.makeWrapper ]
+      ++ pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.shortenPerlShebang;
+
+    src = perlSqitch;
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      for d in bin/sqitch etc lib share ; do
+        # make sure dest alreay exists before symlink
+        # this prevents installing a broken link into the path
+        if [ -e ${perlSqitch}/$d ]; then
+          ln -s ${perlSqitch}/$d $out/$d
+        fi
+      done
+    '' + pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+      shortenPerlShebang $out/bin/sqitch
+    '';
+    dontStrip = true;
+    postFixup = ''
+      wrapProgram $out/bin/sqitch --prefix PERL5LIB : ${
+        pkgs.perlPackages.makeFullPerlPath modules
+      }
+    '';
+
+    meta = {
+      inherit (perlSqitch.meta) description homepage license platforms;
+    };
+  };
+}
