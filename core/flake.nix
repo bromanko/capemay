b8b34c525669111a5a8365529fe21155cad93901
Brian Romanko <hello@bromanko.com>
2022-10-01 14:54:58 -0700

    WIP generating nix deps for a project

--- core/flake.nix
+++ core/flake.nix
@@ -15,16 +15,50 @@
           inherit system;
           overlays = [ (import ../nix/overlays/sqitch.nix) ];
         });
+
+      # buildDotnetModule = project: { };
     in {
-      packages = forAllSystems
-        (system: let pkgs = nixpkgsFor.${system}; in { default = { }; });
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in with pkgs; {
+          CapeMay.Domain = buildDotnetModule rec {
+            pname = "CapeMay.Domain";
+            version = "0.0.1";
+            src = ./.;
+            projectFile = "src/" + pname + "/" + pname + ".fsproj";
+            nugetDeps = ./nix + "/${pname}" + ".deps.nix";
+            dotnet-sdk = dotnetCorePackages.sdk_6_0;
+            buildInputs = [ dotnetPackages.Paket ];
+          };
+          adminServer = buildDotnetModule {
+            pname = "CapeMay Admin Server";
+            version = "0.0.1";
+            src = ./.;
+            projectFile =
+              "src/CapeMay.Admin.Server/CapeMay.Admin.Server.fsproj";
+            nugetDeps = ./nix/CapeMay.Admin.Server.deps.nix;
+            dotnet-sdk = dotnetCorePackages.sdk_6_0;
+            executables = [ "CapeMay.Admin.Server" ];
+            runtimeDeps = [ sqlite ];
+          };
+        });
 
       devShells = forAllSystems (system:
         let pkgs = nixpkgsFor.${system};
         in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ dotnet-sdk mono sqlite sqitch just ];
-          };
+          default = with pkgs;
+            mkShell {
+              packages = [
+                dotnetCorePackages.sdk_6_0
+                mono
+                sqlite
+                sqitch
+                just
+                flyctl
+                nuget-to-nix
+                nixpacks
+              ];
+            };
         });
     };
 }
