281b387183d6ff077a12fc508ec29ac0ce3b2e78
Brian Romanko <hello@bromanko.com>
2022-10-02 14:33:29 -0400

    WIP flake building
    Vp.FSharp.Sql.Sqlite is building

--- core/flake.nix
+++ core/flake.nix
@@ -16,30 +16,32 @@
           overlays = [ (import ../nix/overlays/sqitch.nix) ];
         });
 
-      # buildDotnetModule = project: { };
+      buildCMDotnetModule = (attrs:
+        let
+          projectFile = attrs.projectFile or ("src/" + attrs.pname + "/"
+            + attrs.pname + ".fsproj");
+        in attrs.pkgs.buildDotnetModule {
+          pname = attrs.pname;
+          version = attrs.version or "0.0.1";
+          src = ./.;
+          projectFile = projectFile;
+          nugetDeps = ./nix + "/${attrs.pname}" + ".deps.nix";
+          dotnet-sdk = attrs.pkgs.dotnetCorePackages.sdk_6_0;
+        });
     in {
       packages = forAllSystems (system:
         let pkgs = nixpkgsFor.${system};
-        in with pkgs; {
-          CapeMay.Domain = buildDotnetModule rec {
+        in {
+          Vp.FSharp.Sql.Sqlite = buildCMDotnetModule {
+            inherit pkgs;
+            pname = "Vp.FSharp.Sql.Sqlite";
+            projectFile =
+              "src/Vp.FSharp.Sql.Sqlite/Vp.FSharp.Sql.Sqlite/Vp.FSharp.Sql.Sqlite.fsproj";
+          };
+          CapeMay.Domain = buildCMDotnetModule {
+            inherit pkgs;
             pname = "CapeMay.Domain";
             version = "0.0.1";
-            src = ./.;
-            projectFile = "src/" + pname + "/" + pname + ".fsproj";
-            nugetDeps = ./nix + "/${pname}" + ".deps.nix";
-            dotnet-sdk = dotnetCorePackages.sdk_6_0;
-            buildInputs = [ dotnetPackages.Paket ];
-          };
-          adminServer = buildDotnetModule {
-            pname = "CapeMay Admin Server";
-            version = "0.0.1";
-            src = ./.;
-            projectFile =
-              "src/CapeMay.Admin.Server/CapeMay.Admin.Server.fsproj";
-            nugetDeps = ./nix/CapeMay.Admin.Server.deps.nix;
-            dotnet-sdk = dotnetCorePackages.sdk_6_0;
-            executables = [ "CapeMay.Admin.Server" ];
-            runtimeDeps = [ sqlite ];
           };
         });
 
