5e9855693fb89729e0e574d4655e642c70e2081b
Brian Romanko <hello@bromanko.com>
2022-10-09 09:49:18 -0700

    Switch back to Paket

--- core/flake.nix
+++ core/flake.nix
@@ -13,46 +13,20 @@
       nixpkgsFor = forAllSystems (system:
         import nixpkgs {
           inherit system;
-          overlays = [
-            (import ../nix/overlays/sqitch.nix)
-            (import ../nix/overlays/dotnet.nix)
-          ];
+          overlays = [ (import ../nix/overlays/sqitch.nix) ];
         });
+
+      version = "0.0.1";
     in {
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          Vp.FSharp.Sql.Sqlite = pkgs.buildCmDotnetModule rec {
-            name = "Vp.FSharp.Sql.Sqlite";
-            src = ./.;
-            nugetDeps = ./nix + "/${name}" + ".deps.nix";
-            projectFile =
-              "src/Vp.FSharp.Sql.Sqlite/Vp.FSharp.Sql.Sqlite/Vp.FSharp.Sql.Sqlite.fsproj";
-            packNupkg = true;
-          };
-          CapeMay.Domain = pkgs.buildPaketDotnetModule rec {
-            name = "CapeMay.Domain";
-            src = ./.;
-            nugetDeps = ./nix + "/${name}" + ".deps.nix";
-            packNupkg = true;
-          };
-        });
+      packages = forAllSystems (system: { });
 
       devShells = forAllSystems (system:
         let pkgs = nixpkgsFor.${system};
         in {
           default = with pkgs;
             mkShell {
-              packages = [
-                dotnetCorePackages.sdk_6_0
-                mono
-                sqlite
-                sqitch
-                just
-                flyctl
-                nuget-to-nix
-                nixpacks
-              ];
+              packages =
+                [ dotnetCorePackages.sdk_6_0 mono sqlite sqitch just flyctl ];
             };
         });
     };
